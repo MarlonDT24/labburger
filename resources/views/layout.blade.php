aefa4e8295ec4752e03712fd342c12a3e787a80d
MarlonDT24 <torresmarlon40@gmail.com>
2025-05-05 23:16:26 +0200

    hasta las vistas de login y signup

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.webp') }}">
+</head>
+
+<body>
+    <div class="wrapper">
+        @include ('partials.header')
+        <div class="container mt-4">
+            <main>
+                @yield('content')
+            </main>
+        </div>
+        @include ('partials.footer')
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
