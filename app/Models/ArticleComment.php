54d4bf38a6403a9dda94aaeee1bfecde372af535
MarlonDT24 <167566196+MarlonDT24@users.noreply.github.com>
2025-06-02 11:34:36 +0200

    Update ArticleComment.php

--- app/Models/ArticleComment.php
+++ app/Models/ArticleComment.php
@@ -20,7 +20,6 @@ class ArticleComment extends Model
     {
         return $this->belongsTo(Article::class);
     }
-
     public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
