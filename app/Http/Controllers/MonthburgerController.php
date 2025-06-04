74f2efd8a265bac84f4960d5b88aca29addf1024
MarlonDT24 <167566196+MarlonDT24@users.noreply.github.com>
2025-06-04 12:28:45 +0200

    Update MonthburgerController.php

--- app/Http/Controllers/MonthburgerController.php
+++ app/Http/Controllers/MonthburgerController.php
@@ -40,9 +40,8 @@ class MonthburgerController extends Controller
         $monthburger->name = $name;
         $monthburger->description = $description;
         $monthburger->ingredients = json_encode($ingredients);
-         $monthburger->user_id = Auth::id();
+        $monthburger->user_id = Auth::id();
         $monthburger->save();
-
         return redirect()->route('monthburgers.show', $monthburger->id);
     }
 
@@ -52,27 +51,6 @@ class MonthburgerController extends Controller
     public function show(Monthburger $monthburger)
     {
         return view('monthburgers.show', compact('monthburger'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Monthburger $monthburger)
-    {
-        return view('monthburgers.edit', compact('monthburger'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Monthburger $monthburger)
-    {
-        $monthburger->product_id = $request->input('product_id');
-        $monthburger->date = $request->input('date');
-        $monthburger->ingredients = $request->input('ingredients');
-        $monthburger->save();
-
-        return redirect()->route('monthburgers.show', $monthburger->id);
     }
 
     /**
