b4f69ca609587f094ec493f1c95ca5cb6602adf7
MarlonDT24 <167566196+MarlonDT24@users.noreply.github.com>
2025-06-04 12:18:48 +0200

    Update MonthBurger.php

--- app/Models/MonthBurger.php
+++ app/Models/MonthBurger.php
@@ -11,27 +11,18 @@ class Monthburger extends Model
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'description',
         'ingredients',
         'user_id',
     ];
-
     protected $casts = [
         //De esa manera se pueden manejar los ingredientes como un array json
         'ingredients' => 'array',
     ];
-
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-
 }
